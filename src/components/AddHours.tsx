71789e8d03ad694fa5f2ea189cebeb63c2307a3f
Prince Sodhi <princesodhi@hotmail.com>
2021-01-17 22:39:28 +0100

    feat: add additional hours feature

--- src/components/AddHours.tsx
+++ src/components/AddHours.tsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { DoorClosed, DoorOpenFill } from "react-bootstrap-icons";
+import {
+  Clock,
+  DoorClosed,
+  DoorOpenFill,
+  XCircle,
+} from "react-bootstrap-icons";
 
 import TimeField from "react-simple-timefield";
 
@@ -9,7 +14,11 @@ export type HoursType = {
   day: number;
   from: string;
   to: string;
-  isClosed?: boolean;
+  isClosed: boolean;
+  additionalHours?: {
+    from: string;
+    to: string;
+  } | null;
 };
 export type MergedHours = {
   days: number[];
@@ -22,13 +31,55 @@ const AddHours: React.FC<{
   mergedOpeningHours: (mergedHours: MergedHours[]) => void;
 }> = ({ mergedOpeningHours }) => {
   const [hours, setHours] = useState<HoursType[]>([
-    { day: 0, from: "09:00", to: "12:00", isClosed: false },
-    { day: 1, from: "09:00", to: "12:00", isClosed: false },
-    { day: 2, from: "09:00", to: "13:00", isClosed: false },
-    { day: 3, from: "09:00", to: "13:00", isClosed: false },
-    { day: 4, from: "10:00", to: "14:00", isClosed: false },
-    { day: 5, from: "00:00", to: "00:00", isClosed: true },
-    { day: 6, from: "00:00", to: "00:00", isClosed: true },
+    {
+      day: 0,
+      from: "09:00",
+      to: "12:00",
+      isClosed: false,
+      additionalHours: null,
+    },
+    {
+      day: 1,
+      from: "09:00",
+      to: "12:00",
+      isClosed: false,
+      additionalHours: null,
+    },
+    {
+      day: 2,
+      from: "09:00",
+      to: "13:00",
+      isClosed: false,
+      additionalHours: null,
+    },
+    {
+      day: 3,
+      from: "09:00",
+      to: "13:00",
+      isClosed: false,
+      additionalHours: null,
+    },
+    {
+      day: 4,
+      from: "10:00",
+      to: "14:00",
+      isClosed: false,
+      additionalHours: null,
+    },
+    {
+      day: 5,
+      from: "00:00",
+      to: "00:00",
+      isClosed: true,
+      additionalHours: null,
+    },
+    {
+      day: 6,
+      from: "00:00",
+      to: "00:00",
+      isClosed: true,
+      additionalHours: null,
+    },
   ]);
 
   const handleOnHoursChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -38,31 +89,40 @@ const AddHours: React.FC<{
 
     let newHours = [...hours];
 
+    const isClosed = hours[idx].from === "00:00" && hours[idx].to === "00:00";
+
     newHours[idx] = {
       ...newHours[idx],
       from: hours[idx].from,
       to: hours[idx].to,
       [slot]: value,
-      isClosed: false,
+      isClosed,
     };
 
     setHours(newHours);
   };
 
-  const handleOnFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const days: {
       days: number[];
       from: string;
       to: string;
       isClosed?: boolean;
+      additionalHours?: {
+        from: string;
+        to: string;
+      } | null;
     }[] = [];
 
     const filteredArr: HoursType[] = hours.reduce(
       (acc: any, current: HoursType) => {
         const x = acc.find(
           (hour: HoursType) =>
-            hour?.from === current.from && hour?.to === current.to
+            hour?.from === current.from &&
+            hour?.to === current.to &&
+            hour?.additionalHours?.from === current?.additionalHours?.from &&
+            hour?.additionalHours?.to === current?.additionalHours?.to
         );
 
         return !x ? acc.concat([current]) : acc;
@@ -72,7 +132,11 @@ const AddHours: React.FC<{
 
     filteredArr.map((x) => {
       const results = hours.filter(
-        (item) => item?.from === x.from && item?.to === x.to
+        (item) =>
+          item?.from === x.from &&
+          item?.to === x.to &&
+          item?.additionalHours?.from === x.additionalHours?.from &&
+          item?.additionalHours?.to === x.additionalHours?.to
       );
 
       const isClosed = results[0].from === "00:00" && results[0].to === "00:00";
@@ -82,6 +146,7 @@ const AddHours: React.FC<{
         from: results[0].from,
         to: results[0].to,
         isClosed,
+        additionalHours: results[0].additionalHours || null,
       });
 
       return null;
@@ -103,8 +168,35 @@ const AddHours: React.FC<{
       from: isClosed ? "00:00" : "09:00",
       to: isClosed ? "00:00" : "17:00",
       isClosed,
+      additionalHours: null,
     };
 
+    setHours(newHours);
+  };
+
+  const emptyAdditionalHours = {
+    from: "13:00",
+    to: "17:00",
+  };
+
+  const addAdditionalHours = (hour: HoursType, index: number) => {
+    const newHours = [...hours];
+
+    newHours[index] = {
+      ...newHours[index],
+      ...hour,
+      additionalHours: emptyAdditionalHours,
+    };
+    setHours(newHours);
+  };
+
+  const removeAdditionalHours = (index: number) => {
+    const newHours = [...hours];
+
+    newHours[index] = {
+      ...newHours[index],
+      additionalHours: null,
+    };
     setHours(newHours);
   };
 
@@ -112,68 +204,132 @@ const AddHours: React.FC<{
     <>
       <h3>Add opening hours</h3>
       <div>
-        <form onSubmit={(e) => handleOnFormSubmit(e)}>
+        <form onSubmit={(e) => handleOnSubmit(e)}>
           {hours.map((hour, i) => {
             const { day, isClosed } = hour;
+
             return (
-              <div className="row" key={day}>
-                <div className="col font-weight-bold">{DAY_LOOKUP[day]}</div>
-                <div className="col-4">
-                  <div className="input-group mb-3">
-                    <TimeField
-                      value={hour.from}
-                      onChange={handleOnHoursChange}
-                      input={
-                        <input
-                          type="text"
-                          className="form-control"
-                          id={`${i}`}
-                          slot="from"
+              <React.Fragment key={day}>
+                <div className="row">
+                  <div className="col-2 font-weight-bold">
+                    {DAY_LOOKUP[day]}
+                  </div>
+                  <div className="col-4">
+                    <div className="input-group mb-3">
+                      <TimeField
+                        value={hour.from}
+                        onChange={handleOnHoursChange}
+                        input={
+                          <input
+                            type="text"
+                            className="form-control"
+                            id={`${i}`}
+                            slot="from"
+                          />
+                        }
+                        style={{
+                          width: "100%",
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <div className="col-4">
+                    <div className="input-group mb-3">
+                      <TimeField
+                        value={hour.to}
+                        onChange={handleOnHoursChange}
+                        input={
+                          <input
+                            type="text"
+                            className="form-control"
+                            id={`${i}`}
+                            slot="to"
+                          />
+                        }
+                        style={{
+                          width: "100%",
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <div className="col-2 pl-0">
+                    <span className="btn p-0">
+                      {isClosed ? (
+                        <DoorClosed
+                          color="royalblue"
+                          size={21}
+                          onClick={() => handleOnShopClose(hour, false, i)}
                         />
-                      }
-                      style={{
-                        width: "100%",
-                      }}
-                    />
+                      ) : (
+                        <DoorOpenFill
+                          color="royalblue"
+                          size={21}
+                          onClick={() => handleOnShopClose(hour, true, i)}
+                        />
+                      )}
+                    </span>
+                    <span className="btn p-0">
+                      {!isClosed && !hour?.additionalHours && (
+                        <Clock
+                          color="royalblue"
+                          size={18}
+                          onClick={() => addAdditionalHours(hour, i)}
+                        />
+                      )}
+                    </span>
                   </div>
                 </div>
-                <div className="col-4">
-                  <div className="input-group mb-3">
-                    <TimeField
-                      value={hour.to}
-                      onChange={handleOnHoursChange}
-                      input={
-                        <input
-                          type="text"
-                          className="form-control"
-                          id={`${i}`}
-                          slot="to"
+                {hour?.additionalHours && (
+                  <div className="row">
+                    <div className="col-2"></div>
+                    <div className="col-4">
+                      <div className="input-group mb-3">
+                        <TimeField
+                          value={hour.additionalHours.from}
+                          input={
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="as"
+                              slot="to"
+                            />
+                          }
+                          style={{
+                            width: "100%",
+                          }}
                         />
-                      }
-                      style={{
-                        width: "100%",
-                      }}
-                    />
+                      </div>
+                    </div>
+                    <div className="col-4">
+                      <div className="input-group mb-3">
+                        <TimeField
+                          value={hour.additionalHours.to}
+                          input={
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="as"
+                              slot="to"
+                            />
+                          }
+                          style={{
+                            width: "100%",
+                          }}
+                        />
+                      </div>
+                    </div>
+                    <div className="col-2 pl-0">
+                      <span className="btn p-0">
+                        <XCircle
+                          color="#730000"
+                          size={18}
+                          onClick={() => removeAdditionalHours(i)}
+                        />
+                      </span>
+                    </div>
                   </div>
-                </div>
-                <div className="col-1 pl-0">
-                  <span className="btn p-0">
-                    {isClosed ? (
-                      <DoorClosed
-                        color="royalblue"
-                        size={21}
-                        onClick={() => handleOnShopClose(hour, false, i)}
-                      />
-                    ) : (
-                      <DoorOpenFill
-                        color="royalblue"
-                        size={21}
-                        onClick={() => handleOnShopClose(hour, true, i)}
-                      />
-                    )}
-                  </span>
-                </div>
-              </div>
+                )}
+              </React.Fragment>
             );
           })}
           <div className="row">
